e2982aff10df77e1f6f49228bd45dac8528cc391
saahil-mahato <saahilmahato11@gmail.com>
2024-10-03 10:10:56 +0545

    chore: remove boilerplate

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,25 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.use(PrimeVue, {
+    // Default theme configuration
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+ });
 
 app.use(createPinia())
 app.use(router)
