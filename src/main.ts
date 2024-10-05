2528899b470d4cc49426fa5f4174118bd1189c1a
saahil-mahato <saahilmahato11@gmail.com>
2024-10-05 17:08:30 +0545

    refactor: remove primevue components

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import PrimeVue from 'primevue/config';
-import Lara from '@primevue/themes/lara';
-
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(PrimeVue, {
-    // Default theme configuration
-    theme: {
-        preset: Lara,
-        options: {
-            prefix: 'p',
-            darkModeSelector: 'system',
-            cssLayer: false
-        }
-    }
- });
 
 app.use(createPinia())
 app.use(router)
