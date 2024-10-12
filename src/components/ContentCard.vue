36df8f0b60b0d9cbea81b6150b7d41ce0acdbf17
Saahil Mahato <115351000+saahil-mahato@users.noreply.github.com>
2024-10-12 18:20:52 +0545

    feat: component for card (#8)

---
+++ src/components/ContentCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container" @click="handleClick">
+    <h1 class="heading">{{ heading }}</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+/**
+ * Props for the Card component.
+ */
+const props = defineProps<{
+  /**
+   * The heading or title of the card.
+   */
+  heading: string;
+
+  /**
+   * The function to call when the card is clicked.
+   * @param event - The click event.
+   */
+  clickEvent?: (event: MouseEvent) => void;
+}>();
+
+/**
+ * Handles the click event by calling the provided clickEvent function.
+ * @param event - The click event.
+ */
+const handleClick = (event: MouseEvent) => {
+  if (props.clickEvent) {
+    props.clickEvent(event);
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  @apply flex flex-col p-6 rounded-xl text-[var(--black-color)] bg-[var(--white-color)]
+}
+
+.heading {
+  @apply text-xl text-center font-bold;
+}
+</style>
