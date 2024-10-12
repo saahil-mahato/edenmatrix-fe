73e4e4b24117f433f6946d1f9a94baaf401b2bf0
Saahil Mahato <115351000+saahil-mahato@users.noreply.github.com>
2024-10-12 17:45:37 +0545

    feat: create a button component (#6)
    * feat: create a button component
    
    * refactor: use typescript and document code

---
+++ src/components/TextButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <button :class="buttonClass" @click="handleClick">
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue';
+
+/**
+ * Props for the Button component.
+ */
+ const props = defineProps<{
+  /**
+   * The type of the button, either 'primary' or 'secondary'.
+   */
+  type: 'primary' | 'secondary';
+  
+  /**
+   * The text to display inside the button.
+   */
+  text: string;
+  
+  /**
+   * The function to call when the button is clicked.
+   * @param event - The click event.
+   */
+  clickEvent: (event: MouseEvent) => void;
+}>();
+
+/**
+ * Computed property to determine the button's CSS class based on its type.
+ */
+ const buttonClass = computed(() => {
+  return props.type === 'primary' ? 'btn-primary' : 'btn-secondary';
+});
+
+/**
+ * Handles the click event by calling the provided clickEvent function.
+ * @param event - The click event.
+ */
+const handleClick = (event: MouseEvent) => {
+  props.clickEvent(event);
+};
+</script>
+
+<style scoped>
+.btn-primary {
+  @apply p-3 text-[var(--white-color)] bg-[var(--primary-color)] rounded-md hover:shadow-2xl hover:bg-[var(--primary-hover-color)]
+}
+</style>
